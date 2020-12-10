a73d1978be4c2d0b191e673393d029cf76c8ac1a
IsaacDSC <isaac8.silva@hotmail.com>
2020-12-10 10:25:37 -0300

    adionando models

--- webPage/blog/admin.py
+++ webPage/blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Client
+from .models import Noticias
+
 
 admin.site.register(Client)
+admin.site .register(Noticias)
 
--- webPage/blog/models.py
+++ webPage/blog/models.py
@@ -6,3 +6,12 @@ class Client(models.Model):
     name = models.CharField(max_length = 80)
     email =models.CharField(max_length = 220)
     dateTime = models.DateTimeField(auto_now_add = True)
+
+
+class Noticias(models.Model):
+    title = models.CharField(max_length = 80)
+    subtitle = models.CharField(max_length = 220)
+    description = models.TextField()
+    location = models.CharField(max_length = 100)
+    date = models.CharField(max_length = 30)
+    dataTime = models.DateTimeField(auto_now_add = True)
